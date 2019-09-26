bb0b3a8ad90ddcd80e0fb23ed0f1b7c652087706
Robin Cole <robmarkcole@gmail.com>
2019-09-26 12:41:37 +0100

    Working test

--- tests/test_london_air_quality.py
+++ tests/test_london_air_quality.py
@@ -6,12 +6,19 @@ import json
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
+this_directory = os.path.join(os.path.dirname(__file__))
+sys.path.insert(0, os.path.abspath(this_directory))
 import london_air_quality as laq
 
 def test_request_data():
+    """
+    Test that the request returns the AI data.
+    """
+    with open(this_directory + '/hourly.json') as json_file:
+        api_hourly_json = json.load(json_file)
+
     with requests_mock.Mocker() as mock_req:
         url = laq.LAQ_HOURLY_URL
-        mock_req.get(url, text=json.loads("tests/hourly.json"))
+        mock_req.get(url, json=api_hourly_json)
         hourly_data_raw = laq.request_data(url)
-        assert len(hourly_data_raw.keys()) == 33
+        assert len(hourly_data_raw['HourlyAirQualityIndex']['LocalAuthority']) == 33
